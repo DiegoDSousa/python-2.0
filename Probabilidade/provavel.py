cb1e3db089a9a67f89dc8b882336333102df4d76
DIEGOSOUSA <sdiego478@gmail.com>
2022-03-20 15:48:18 -0100

    teste

--- Probabilidade/provavel.py
+++ Probabilidade/provavel.py
@@ -3,10 +3,12 @@ from urllib.request import DataHandler
 dados=[32.3,62.2,10.3,22.0,13.1,9.9,11.9,20.0,36.4,23.5,18.0,22.6,20.3,38.3,19.6,27.2,28.9,18.4,27.3,21.7,23.7,13.9,36.3,32.9,
 29.7,25.4,23.8,15.7,17.0,39.2,22.7,29.9,18.3,33.0]
 rol=len(dados)
+dados_organizados=[]
 print("dados brutos: ",dados)
 dados_organizados=dados
 dados_organizados.sort()
-print(dados_organizados)
+
+print("dados organizados: ",dados_organizados)
 print(len(dados))
 c=0
 l_sup=0
@@ -31,14 +33,15 @@ range=l_sup-l_inf
 amplitude=range/nclasses
 amplitude=math.ceil(range/nclasses)
 pontoMedio=(l_sup+l_inf)/2
-print(l_sup,l_inf)
-print(range)
-print(amplitude)
+print("limite superior: ",l_sup," limite inferior: ",l_inf)
+print("range: ",range)
+print("amplitude: ",amplitude)
 escrever=""
 
-print(nclasses)
+print("numero de classes: ",nclasses)
 with open('Probabilidade/resultado.txt','w') as f:
     f.write("Dados: "+ str(dados)+"\n")
+    
     f.write("Dados organizados: "+str(dados_organizados)+"\n")
     f.write("Numero Elementos: "+str(rol)+"\n")
     f.write("Range: "+str(range)+"\n")
@@ -57,18 +60,20 @@ lista_classes=[]
 lista_frequencia=[]
 inicio=dados_organizados[0]
 final=inicio+amplitude
+posicao=0
 while c!=nclasses:
-    a=0
+    a=posicao
     frequencia=0
     lista_classes.append("["+str(inicio)+","+str(final)+"[")
     while a!=rol:
-        if (dados_organizados[a] <= inicio) or (dados_organizados[a]>final):
+        if (dados_organizados[a] >= inicio) and (dados_organizados[a]<final):
             frequencia+=1
         a+=1
+    posicao+=frequencia
     lista_frequencia.append(str(frequencia))
     inicio=final
     final=final+amplitude
+
     c+=1
-print("aa")
-print(lista_classes)
-print(lista_frequencia)
+print("lista classes: ",lista_classes)
+print("frequencias: ",lista_frequencia)
